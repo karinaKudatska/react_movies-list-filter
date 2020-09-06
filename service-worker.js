ed6a2b20892100d569676ee76923259ea8db5729
karinaKudatska <karina.kudatska@gmail.com>
2020-09-06 22:21:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.1172c5891a31274e44c99313f637713b.js"
 );
 
 self.addEventListener('message', (event) => {
